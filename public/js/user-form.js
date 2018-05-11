f7110b1a74022728e45dc72b2dda144fb37fcfad
Jane Jeon <sungil.ahn.19@dartmouth.edu>
2018-05-11 05:57:36 -0400

    revamped user validation and added extensive testing

--- public/js/user-form.js
+++ public/js/user-form.js
@@ -1,11 +1,13 @@
 $(function() {
-    // client-side validation
-    $('form').submit(function(e) {
-        $(this).find('input').each(function() {
-            if (error = user[`validate${_.capitalize($(this).attr('name'))}`]($(this).val())) {
-                swal(error)
-                e.preventDefault()
-            }
-        })
-    })
+	// client-side validation
+	$('form').submit(function(e) {
+		$(this)
+			.find('input')
+			.each(function() {
+				if ((error = user_validate[`${$(this).attr('name')}`]($(this).val()))) {
+					swal(error)
+					e.preventDefault()
+				}
+			})
+	})
 })
---
+++ public/js/validate-user.js
@@ -0,0 +1,1 @@
+../../lib/validate-user.js
